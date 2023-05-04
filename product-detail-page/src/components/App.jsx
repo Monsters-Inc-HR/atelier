b5758ae91896573aa24d8925c3583142a534521c
Andre Bagur <andre.bagur@gmail.com>
2023-05-04 15:32:52 -0500

    added my hello world for questions and answers

--- product-detail-page/src/components/App.jsx
+++ product-detail-page/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import RelatedItems from './relatedItems/relatedItems.jsx';
 import Ratings from './ratingsAndReviews/ratings.jsx';
+import QuestionsAndAnswers from './questionsAndAnswers/QuestionsAndAnswers.jsx'
+
 
 const App = () => {
 
   return (
     <><RelatedItems/>
+    <QuestionsAndAnswers/>
     <Ratings /></>
   )
 }
