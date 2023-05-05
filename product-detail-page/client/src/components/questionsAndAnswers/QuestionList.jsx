182ccf78510c737253fd945ed0e87a4c8572792c
Andre Bagur <andre.bagur@gmail.com>
2023-05-05 13:54:55 -0500

    adds question list component and question entry component

---
+++ product-detail-page/client/src/components/questionsAndAnswers/QuestionList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const QuestionList = () => {
+
+
+
+  return (
+    <div style={{ borderStyle: 'solid', borderColor: 'grey' }}>
+      Question List div
+    </div>
+  )
+}
+
+export default QuestionList
--- product-detail-page/client/src/components/questionsAndAnswers/QuestionsAndAnswers.jsx
+++ product-detail-page/client/src/components/questionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Searchbar from "./Searchbar.jsx"
+import Searchbar from "./Searchbar.jsx";
+import QuestionList from "./QuestionList.jsx";
 
 const QuestionsAndAnswers = () => {
   return (<div style={{ borderStyle: 'solid', borderColor: 'grey' }}>
     Questions and Answers div
     <Searchbar />
+    <QuestionList />
+    <button>Add an answer</button>
     </div>)
 }
 
