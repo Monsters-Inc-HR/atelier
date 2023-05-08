c7956a287140835b930c0df9d331c54bfd2e6bd3
Andre Bagur <andre.bagur@gmail.com>
2023-05-08 09:42:00 -0500

    final changes before commit

--- product-detail-page/client/src/components/questionsAndAnswers/QuestionList.jsx
+++ product-detail-page/client/src/components/questionsAndAnswers/QuestionList.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import QuestionEntry from './QuestionEntry.jsx';
 
 
-//const questions = ['a', 'b', 'c', 'd', 'e'];
+
 
 const QuestionList = ({questions}) => {
 
 
- //console.log(questions);
+
   return (
     <div style={{ borderStyle: 'solid', borderColor: 'blue' }}>
       {questions.map((question, index) => (
