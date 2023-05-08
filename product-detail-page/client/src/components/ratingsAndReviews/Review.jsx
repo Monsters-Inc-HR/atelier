c8ed0261fc5c602cf83cf4f61386cb641bf91306
Alex Caron <alex.caron@gmail.com>
2023-05-08 13:23:08 -0500

    make review display user name and formatted date

--- product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 const Review = ({ review }) => {
   return (
     <div className='rr-review'>
-      <div className='rr-review-header'></div>
+      <div className='rr-review-header'>
+        <div className='rr-stars'>rating: { review.rating }</div>
+        <div className='rr-review-user-date'> {review.reviewer_name}, {format(new Date(review.date), 'MMMM d, y')} </div>
+      </div>
       <div className='rr-review-summary'>{ review.summary }</div>
       <div className='rr-review-body'>{ review.body }</div>
       { review.recommend && <div className='rr-review-recommended'>✔️ I recommend this product.</div> }
