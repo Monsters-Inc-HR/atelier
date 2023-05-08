bf9e364dd8ab0b15bb45ef998dae97c6f6290f87
Alex Caron <alex.caron@gmail.com>
2023-05-08 16:31:21 -0500

    ensure review list children all receive a unique key to keep React happy

--- product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
@@ -6,7 +6,7 @@ const ReviewsList = ({ reviewsData }) => {
   return (
     <div className='rr-reviews-list'>
       <div className='rr-review-sorter'>{ reviewsData.results.length } reviews, sorted by relevance/recency</div>
-      {reviewsData.results.slice(0, numOfDisplayedReviews).map(review => <Review review={ review } />)}
+      {reviewsData.results.slice(0, numOfDisplayedReviews).map(review => <Review key={ review.review_id } review={ review } />)}
       {numOfDisplayedReviews < reviewsData.results.length && <button onClick={ () => setNumOfDisplayedReviews(numOfDisplayedReviews + 2)}>MORE REVIEWS</button>}
       <button>ADD A REVIEW  +</button>
     </div>
