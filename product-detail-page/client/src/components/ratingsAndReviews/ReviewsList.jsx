8c64fa4f87c83ed8234426810c04c1a8c7c34665
Alex Caron <alex.caron@gmail.com>
2023-05-08 11:46:43 -0500

    add some styling to the reviews component overall

--- product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
@@ -4,7 +4,7 @@ import Review from './Review.jsx';
 const ReviewsList = ({ reviewsData }) => {
   const [numOfDisplayedReviews, setNumOfDisplayedReviews] = useState(2);
   return (
-    <div className='rr-review-list'>
+    <div className='rr-reviews-list'>
       <div className='rr-review-sorter'>{ reviewsData.results.length } of reviews, sorted by relevance/recency</div>
       {reviewsData.results.slice(0, numOfDisplayedReviews).map(review => <Review review={ review } />)}
     </div>
