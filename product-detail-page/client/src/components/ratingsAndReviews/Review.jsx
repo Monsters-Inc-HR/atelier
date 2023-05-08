bbc4bf32d1828847a8ad069b4f11377d30cca82d
Alex Caron <alex.caron@gmail.com>
2023-05-08 11:31:42 -0500

    make Reviews List display 2 reviews and pass dynamic data to them

--- product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Review = () => {
+const Review = ({ review }) => {
   return (
     <div className='rr-review'>
       <div className='rr-review-header'></div>
-      <div className='rr-review-summary'></div>
-      <div className='rr-review-body'></div>
+      <div className='rr-review-summary'>{ review.summary }</div>
+      <div className='rr-review-body'>{ review.body }</div>
       <div className='rr-review-recommended'></div>
       <div className='rr-review-seller-response'></div>
       <div className='rr-mark-helpful'></div>
--- product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import Review from './Review.jsx';
 
 const ReviewsList = ({ reviewsData }) => {
+  const [numOfDisplayedReviews, setNumOfDisplayedReviews] = useState(2);
   return (
     <div className='rr-review-list'>
       <div className='rr-review-sorter'>{ reviewsData.results.length } of reviews, sorted by relevance/recency</div>
-      <Review />
-      <Review />
+      {reviewsData.results.slice(0, numOfDisplayedReviews).map(review => <Review review={ review } />)}
     </div>
   );
 };
