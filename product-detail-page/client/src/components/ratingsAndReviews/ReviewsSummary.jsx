5690c305d1a70f403f6e38a5810782065ab08fa0
Alex Caron <alex.caron@gmail.com>
2023-05-08 16:17:35 -0500

    finish star bar component styling

--- product-detail-page/client/src/components/ratingsAndReviews/ReviewsSummary.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/ReviewsSummary.jsx
@@ -4,7 +4,6 @@ import calculateStars from '../../lib/calculateStars.js';
 import calculatePercentage from '../../lib/calculatePercentage.js';
 
 const ReviewsSummary = ({ metaData }) => {
-  console.log(metaData);
   return (
     <div className='rr-summary'>
       <div className='rr-summary-rollup'>
@@ -14,6 +13,10 @@ const ReviewsSummary = ({ metaData }) => {
       <div className='rr-filter-list'>
         <div className='rr-filter-message'>filtering by... remove filters</div>
         <StarBar star={ 5 } ratings={ metaData.ratings }/>
+        <StarBar star={ 4 } ratings={ metaData.ratings }/>
+        <StarBar star={ 3 } ratings={ metaData.ratings }/>
+        <StarBar star={ 2 } ratings={ metaData.ratings }/>
+        <StarBar star={ 1 } ratings={ metaData.ratings }/>
       </div>
       <div className='rr-percentage-recommended'>{calculatePercentage(metaData)}% of reviews recommend this product</div>
       <div className='rr-scale-component'>scale from 1-5</div>
--- product-detail-page/client/src/components/ratingsAndReviews/StarBar.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/StarBar.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const StarBar = ({star, ratings}) => {
   const totalNumOfRatings = Object.values(ratings).reduce((sum, countForEachStar) => sum + parseInt(countForEachStar), 0);
   const ratioToDisplay = parseInt(ratings[star]) / totalNumOfRatings;
-  const positivePercentageString = String.toString(Math.round(ratioToDisplay*100)) + '%';
-  const negativePercentageString = String.toString(Math.round((1 - ratioToDisplay)*100)) + '%';
+  const positivePercentageString = Math.round(ratioToDisplay*100) + '%';
+  const negativePercentageString = Math.round((1 - ratioToDisplay)*100) + '%';
   return (
     <div className='rr-star-bar'>
-      <div>{star} stars</div>
+      <div className='rr-star-bar-text'>{star} star{star > 1 && 's'}</div>
       <div className='rr-star-bar-100'>
         <div className='rr-star-bar-positive' style={{width: positivePercentageString}}></div>
         <div className='rr-star-bar-negative' style={{width: negativePercentageString}}></div>
