7983ef7f5ef3393c994d17720c4ff46cdb531289
Alex Caron <alex.caron@gmail.com>
2023-05-08 12:32:10 -0500

    conditionally render the recommender message in each review

--- product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
@@ -6,7 +6,7 @@ const Review = ({ review }) => {
       <div className='rr-review-header'></div>
       <div className='rr-review-summary'>{ review.summary }</div>
       <div className='rr-review-body'>{ review.body }</div>
-      <div className='rr-review-recommended'></div>
+      { review.recommend && <div className='rr-review-recommended'>✔️ I recommend this product.</div> }
       <div className='rr-review-seller-response'></div>
       <div className='rr-mark-helpful'></div>
     </div>
--- product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
@@ -5,7 +5,7 @@ const ReviewsList = ({ reviewsData }) => {
   const [numOfDisplayedReviews, setNumOfDisplayedReviews] = useState(2);
   return (
     <div className='rr-reviews-list'>
-      <div className='rr-review-sorter'>{ reviewsData.results.length } of reviews, sorted by relevance/recency</div>
+      <div className='rr-review-sorter'>{ reviewsData.results.length } reviews, sorted by relevance/recency</div>
       {reviewsData.results.slice(0, numOfDisplayedReviews).map(review => <Review review={ review } />)}
     </div>
   );
