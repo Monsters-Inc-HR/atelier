43ebe8207a5027395368799a84e7193018d9620d
Alex Caron <alex.caron@gmail.com>
2023-05-09 13:47:55 -0500

    final changes to server file

--- product-detail-page/server/server.js
+++ product-detail-page/server/server.js
@@ -13,6 +13,18 @@ app.get('/products', (req, res) => {
   res.send();
 })
 
+app.get('/productsDetails', (req, res) => {
+  console.log(req.params);
+  // controller.getProducts(req.options.page, req.options.)
+  res.send();
+})
+
+app.get('/productImages', (req, res) => {
+  console.log(req.params);
+  // controller.getProducts(req.options.page, req.options.)
+  res.send();
+})
+
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
