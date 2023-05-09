866f02c2d9d668210a4bf5cdb8bd35b60a8c0d18
rodrigogramitto <rgramitto@gmail.com>
2023-05-09 11:37:14 -0400

    Controller functions implemented

--- product-detail-page/client/src/lib/controller.js
+++ product-detail-page/client/src/lib/controller.js
@@ -4,31 +4,14 @@ require('dotenv').config();
 module.exports = {
   getProducts: (page, count) => {
     return axios({
-        method: 'get',
-        url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/',
-        headers: {'authorization': process.env.API_KEY},
-        params: {
-          page: page,
-          count: count
-        }
-      })
-      .then((products) => {
-        return products.data;
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  },
-  getProducts: (page, count) => {
-    return axios({
-        method: 'get',
-        url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/',
-        headers: {'authorization': process.env.API_KEY},
-        params: {
-          page: page,
-          count: count
-        }
-      })
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/',
+      headers: { 'authorization': process.env.API_KEY },
+      params: {
+        page: page,
+        count: count
+      }
+    })
       .then((products) => {
         return products.data;
       })
@@ -37,75 +20,104 @@ module.exports = {
       })
   },
 
-
-// Get product information
-// takes product ID parameter
-getProductDetails: (productID) => {
-  if (productID === undefined) {
-    console.log('Please input a valid Product ID');
-  }
-  return axios({
-    method: 'get',
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productID}`
-  })
-  .then((productDetails) => {
-    return productDetails;
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-},
-
-
-// Get product styles
-// takes product ID
-getProductStyles: (productID) => {
-
-},
-
-// Get related products
-// takes product ID
-getRelatedProducts: (productID) => {
-
-},
-
-// OPTIONAL: Get product images (i think it would be quite helpful to have a dedicated function for this)
-getProductImages: (productID) => {
-
-},
-
-// Get the full reviews data for a specific product
-const getReviewsData = (productID, page, countPerPage) => {
-  if (!productID) {
-    console.log('Please enter a product id');
-  }
-  return axios({
-    method: 'get',
-    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews',
-    headers: {'Authorization': process.env.API_KEY},
-    params: {
-      product_id: productID
+  // Get product information
+  // takes product ID parameter
+  getProductDetails: (productID) => {
+    if (productID === undefined) {
+      console.log('Please input a valid Product ID');
     }
-  })
-  .then(results => JSON.parse(results))
-  .catch((err) => console.log("there was an error getting reviews meta data: ", err))
-},
+    return axios({
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productID}`,
+      headers: { 'authorization': process.env.API_KEY }
+    })
+      .then((productDetails) => {
+        return productDetails.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 
-// Get the reviews metadata for a specific product
-  const getReviewsMetaData = (productID) => {
+
+  // Get product styles
+  // takes product ID
+  getProductStyles: (productID) => {
+    if (productID === undefined) {
+      console.log('Please input a valid Product ID');
+    }
+    return axios({
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productID}/styles`,
+      headers: { 'authorization': process.env.API_KEY }
+    })
+      .then((productStyles) => {
+        return productStyles.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+
+  // Get related products
+  // takes product ID
+  getRelatedProducts: (productID) => {
+    if (productID === undefined) {
+      console.log('Please input a valid Product ID');
+    }
+    return axios({
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productID}/related`,
+      headers: { 'authorization': process.env.API_KEY }
+    })
+      .then((productStyles) => {
+        return productStyles.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+
+  // OPTIONAL: Get product images (i think it would be quite helpful to have a dedicated function for this)
+  // getProductImages: (productID) => {
+  //   return 'Sorry this function is not operational yet';
+  // },
+
+  // Get the full reviews data for a specific product
+  getReviewsData: (productID, page, count) => {
+    if (!productID) {
+      console.log('Please enter a product id');
+    }
+    return axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews',
+      headers: { 'Authorization': process.env.API_KEY },
+      params: {
+        page: page,
+        count: count,
+        product_id: productID
+      }
+    })
+      .then(results => results.data)
+      .catch((err) => console.log("there was an error getting reviews meta data: ", err))
+  },
+
+  // Get the reviews metadata for a specific product
+  getReviewsMetaData: (productID) => {
     if (!productID) {
       console.log('Please enter a product id');
     }
     return axios({
       method: 'get',
       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/meta',
-      headers: {'Authorization': process.env.API_KEY},
+      headers: { 'Authorization': process.env.API_KEY },
       params: {
         product_id: productID
       }
     })
-    .then(results => results.data)
-    .catch((err) => console.log("there was an error getting reviews meta data: ", err))
-  }
-
+      .then(results => results.data)
+      .catch((err) => console.log("there was an error getting reviews meta data: ", err))
+  },
 };
+
+module.exports.getProductStyles(37315).then((result) => console.log(result));
