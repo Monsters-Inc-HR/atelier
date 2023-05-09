9d42d932e0620cddd5764cf920f24c142573b646
Alex Caron <alex.caron@gmail.com>
2023-05-09 16:50:54 -0500

    create a sort by selector

--- product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/ReviewsList.jsx
@@ -3,9 +3,16 @@ import Review from './Review.jsx';
 
 const ReviewsList = ({ reviews }) => {
   const [displayCount, setDisplayCount] = useState(2);
+
   return (
     <div className='rr-reviews-list'>
-      <div className='rr-review-sorter'>{ reviews.length } reviews, sorted by relevance/recency</div>
+      <div className='rr-review-sorter'>{ reviews.length } reviews, <label for='sort-select'>sorted by most</label>
+        <select style={{display: 'inline'}} name='sort-by' id='sort-select'>
+          <option value='relevance' selected='true'>relevant</option>
+          <option value='helpfulness'>helpful</option>
+          <option value='recency'>recent</option>
+        </select>
+      </div>
       {reviews.slice(0, displayCount)
         .map(review => <Review key={ review.review_id } review={ review } />)}
       {displayCount < reviews.length && <button onClick={ () => setDisplayCount(displayCount + 2)}>MORE REVIEWS</button>}
