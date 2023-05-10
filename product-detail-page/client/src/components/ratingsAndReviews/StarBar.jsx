f92d6a055b2315c676dddf92ede68bdac2e7274e
Alex Caron <alex.caron@gmail.com>
2023-05-10 15:49:32 -0500

    get star bar to display a fixed number of positive stars

--- product-detail-page/client/src/components/ratingsAndReviews/StarBar.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/StarBar.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const StarBar = ({ rating }) => {
+  const stars = Array(rating).fill(<FontAwesomeIcon icon={ icon({name: 'star'}) } />);
   return (
-    <div className='rr-star-bar'>{ rating }</div>
+    <div className='rr-star-bar'>
+      { stars }
+    </div>
   )
 }
 
