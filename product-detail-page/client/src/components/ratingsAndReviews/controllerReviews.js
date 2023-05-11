81f3c9a37196fa38f6a62d7d11d24548c8c4d3cf
Alex Caron <alex.caron@gmail.com>
2023-05-11 15:04:52 -0500

    fix a conditional statement in getPageOfReviews function

--- product-detail-page/client/src/components/ratingsAndReviews/controllerReviews.js
+++ product-detail-page/client/src/components/ratingsAndReviews/controllerReviews.js
@@ -4,8 +4,8 @@ module.exports = {
   getPageOfReviews: (productID = 37311, page, count) => {
     // check inputs before making the request
     if (typeof productID !== 'number' || typeof parseInt(productID) !== number) productID = 37311;
-    if page & (typeof page !== 'number' || typeof parseInt(page) !== number) page = 1;
-    if count & (typeof count !== 'number' || typeof parseInt(count) !== number) count = 5;
+    if page && (typeof page !== 'number' || typeof parseInt(page) !== number) page = 1;
+    if count && (typeof count !== 'number' || typeof parseInt(count) !== number) count = 5;
 
     return axios.get('http://localhost:3000/reviews', {
       params: {
