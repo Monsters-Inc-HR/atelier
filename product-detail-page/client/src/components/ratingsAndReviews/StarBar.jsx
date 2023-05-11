b89b78f9c451a01a5b19bae5f00a71f615fa14b2
Alex Caron <alex.caron@gmail.com>
2023-05-11 10:28:35 -0500

    add clip-path to css stylesheet for 25% star

--- product-detail-page/client/src/components/ratingsAndReviews/StarBar.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/StarBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const StarBar = ({ rating }) => {
-  const stars = Array(rating).fill(<FontAwesomeIcon icon={ icon({name: 'star'}) } />);
+  const stars = Array(5);
+  stars.fill(<FontAwesomeIcon icon={ icon({name: 'star', style: 'solid'}) } />, 0, rating);  // fill from 0 up to rating index with solid stars
+  stars.fill(<FontAwesomeIcon className='rr-star-filled-25' icon={ icon({name: 'star', style: 'regular'}) } />, rating);   // fill from rating index to end with empty stars
+
+  // useEffect(() => {
+  //   const clip = document.createElementNS('http://www.w3.org/2000/svg', 'clipPath');
+  //   clip.id = 'rr-clipper-1';
+  //   const rect = document.createElement('rect');
+  //   rect.setAttribute('x', 0);
+  //   rect.setAttribute('y', 0);
+  //   rect.setAttribute('width', '75%');
+  //   rect.setAttribute('height', '100%');
+  //   clip.appendChild(rect);
+
+  //   // const use = document.createElement('use');
+  //   // use.setAttribute('clip-path', 'url(#rr-clipper-1)');
+  //   // use.setAttribute('href', '1')
+
+  //   const targetSVG = document.getElementById('1');
+  //   targetSVG.querySelector('path').setAttribute('clip-path', 'url(#rr-clipper-1)');
+  //   targetSVG.appendChild(clip);
+  //   //targetSVG.appendChild(use);
+  // }, []);
+
   return (
     <div className='rr-star-bar'>
       { stars }
