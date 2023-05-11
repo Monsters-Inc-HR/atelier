857f7e4e4f541b7f3155a8b98bf79c55b77cf855
Alex Caron <alex.caron@gmail.com>
2023-05-11 14:57:17 -0500

    update endpoints for reviews on our server

---
+++ product-detail-page/client/src/components/ratingsAndReviews/controllerReviews.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+module.exports = {
+  getPageOfReviews: (productID = 37311, page, count) => {
+    // check inputs before making the request
+    if (typeof productID !== 'number' || typeof parseInt(productID) !== number) productID = 37311;
+    if page & (typeof page !== 'number' || typeof parseInt(page) !== number) page = 1;
+    if count & (typeof count !== 'number' || typeof parseInt(count) !== number) count = 5;
+
+    return axios.get('http://localhost:3000/reviews', {
+      params: {
+        product_id: productID,
+        page: page,
+        count: count
+      }
+    })
+    .then((reviews) => {
+      return reviews.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  },
+  getReviewsMetaData: (productID = 37311) => {
+    // check inputs before making the request
+    if (typeof productID !== 'number' || typeof parseInt(productID) !== number) productID = 37311;
+    return axios.get('http://localhost:3000/reviews/meta', {
+      params: {
+        product_id: productID,
+      }
+    })
+    .then((reviewsMeta) => {
+      return reviewsMeta.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+};
--- product-detail-page/server/server.js
+++ product-detail-page/server/server.js
@@ -55,6 +55,27 @@ app.get('/styles', (req, res) => {
   })
 })
 
+app.get('/reviews', (req, res) => {
+  let paramsString = req.url.split('?')[1];
+  let paramsTuples = paramsString.split('&').map((pair) => pair.split('='));
+  let productID = paramsTuples.find([name, val] => name === 'product_id')[1];
+  let page = paramsTuples.find([name, val] => name === 'page');  // will be undefined if this param has not been provided
+  if (page) page = page[1];  // sets this to parameter value only if it exists
+  let count = paramsTuples.find([name, val] => name === 'count');  // will be undefined if this param has not been provided
+  if (count) count = count[1];  // sets this to parameter value only if it exists
+  controller.getReviewsData(productID, page, count)
+    .then(results => res.send(results))
+    .catch(err => res.send(err));
+})
+
+app.get('/reviews/meta', (req, res) => {
+  let paramsString = req.url.split('?')[1];
+  let paramsTuples = paramsString.split('&').map((pair) => pair.split('='));
+  let productID = paramsTuples.find([name, val] => name === 'product_id')[1];
+  controller.getReviewsMetaData(productID)
+    .then(results => res.send(results))
+    .catch(err => res.send(err));
+})
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
