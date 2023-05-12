748aa1e931d922ae82a04ee59f908f6d83ccb823
Alex Caron <alex.caron@gmail.com>
2023-05-12 11:35:23 -0500

    set up show more for review body over 250 chars

--- product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
@@ -4,7 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Review = ({ review }) => {
-  //const [, ]
+  const maxCollapsedChars = 250;
+  const [collapsed, setCollapsed] = useState(review.body.length > maxCollapsedChars);
   return (
     <div className='rr-review'>
       <div className='rr-review-header'>
@@ -12,7 +13,13 @@ const Review = ({ review }) => {
         <div className='rr-review-user-date'> {review.reviewer_name}, {format(new Date(review.date), 'MMMM d, y')} </div>
       </div>
       <div className='rr-review-summary'>{ review.summary }</div>
-      <div className='rr-review-body'>{ review.body }</div>
+      {
+        collapsed ?
+        <div className='rr-review-body'>{ review.body.slice(0, maxCollapsedChars) }
+          <button className='rr-review-show-more' onClick={() => setCollapsed(false) }>SHOW MORE</button>
+        </div> :
+        <div className='rr-review-body'>{ review.body }</div>
+      }
       { review.recommend && <div className='rr-review-recommended'><FontAwesomeIcon icon={ icon({name: 'check', style: 'solid'}) } /> I recommend this product.</div> }
       { review.response && <div className='rr-review-seller-response'><b>Seller response:</b><br/><br/>{ review.response }</div> }
       <div className='rr-mark-helpful'></div>
