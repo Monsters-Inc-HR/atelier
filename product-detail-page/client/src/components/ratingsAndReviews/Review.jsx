24d7a803d86dfdb51f4471396f711107325e1a53
Alex Caron <alex.caron@gmail.com>
2023-05-12 11:11:04 -0500

    add checkmark icon and set server port back to 3000

--- product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { format } from 'date-fns';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Review = ({ review }) => {
+  //const [, ]
   return (
     <div className='rr-review'>
       <div className='rr-review-header'>
@@ -10,7 +13,7 @@ const Review = ({ review }) => {
       </div>
       <div className='rr-review-summary'>{ review.summary }</div>
       <div className='rr-review-body'>{ review.body }</div>
-      { review.recommend && <div className='rr-review-recommended'>✔️ I recommend this product.</div> }
+      { review.recommend && <div className='rr-review-recommended'><FontAwesomeIcon icon={ icon({name: 'check', style: 'solid'}) } /> I recommend this product.</div> }
       { review.response && <div className='rr-review-seller-response'><b>Seller response:</b><br/><br/>{ review.response }</div> }
       <div className='rr-mark-helpful'></div>
       <hr />
