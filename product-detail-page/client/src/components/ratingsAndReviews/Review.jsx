f60958227175714b027777b115aaf2dfe3720c5a
Alex Caron <alex.caron@gmail.com>
2023-05-12 17:03:19 -0500

    fix conditional rendering and display images in review card

--- product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/Review.jsx
@@ -6,22 +6,32 @@ import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Review = ({ review }) => {
   const maxCollapsedChars = 250;
+  const maxTitleChars = 60;
   const [collapsed, setCollapsed] = useState(review.body.length > maxCollapsedChars);
+  const shortSummary = (review.summary.length <= maxTitleChars) ? review.summary : review.summary.slice(0, review.summary.indexOf(' ', maxTitleChars)) + '...';
+  const restOfSummary = (review.summary.length <= maxTitleChars) ? null : '...' + review.summary.slice(review.summary.indexOf(' ', maxTitleChars));
+  const sortedPhotos = review.photos.sort((a, b) => a.id < b.id);
+
   return (
     <div className='rr-review'>
       <div className='rr-review-header'>
         <StarBar rating={ Number.parseFloat(review.rating).toFixed(2) } />
         <div className='rr-review-user-date'> {review.reviewer_name}, {format(new Date(review.date), 'MMMM d, y')} </div>
       </div>
-      <div className='rr-review-summary'>{ review.summary }</div>
-      {
-        collapsed ?
-        <div className='rr-review-body'>{ review.body.slice(0, maxCollapsedChars) }
-          <button className='rr-review-show-more' onClick={() => setCollapsed(false) }>SHOW MORE</button>
-        </div> :
-        <div className='rr-review-body'>{ review.body }</div>
-      }
+      <div className='rr-review-summary'>{ shortSummary }</div>
+      <div className='rr-review-body'>
+        { restOfSummary && <p>{ restOfSummary }</p> }
+        <p>{ collapsed ? review.body.slice(0, review.body.indexOf(' ', maxCollapsedChars)) : review.body }</p>
+        { review.body.length > maxCollapsedChars &&
+        <button className='rr-review-show-more' onClick={ () => setCollapsed(!collapsed) }>
+          { collapsed ? 'SHOW MORE' : 'SHOW LESS' }
+        </button>
+        }
+      </div>
       { review.recommend && <div className='rr-review-recommended'><FontAwesomeIcon icon={ icon({name: 'check', style: 'solid'}) } /> I recommend this product.</div> }
+      <div className='rr-review-photos'>
+        { sortedPhotos.map(p => <img className='rr-review-image' key={p.id} src={`${p.url}`} />) }
+      </div>
       { review.response && <div className='rr-review-seller-response'><b>Seller response:</b><br/><br/>{ review.response }</div> }
       <div className='rr-mark-helpful'></div>
       <hr />
