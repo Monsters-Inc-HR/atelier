36f83b900e80468166414848e3f6383cdf822165
Alex Caron <alex.caron@gmail.com>
2023-05-13 13:43:52 -0500

    render series of scale components based on actual data

--- product-detail-page/client/src/components/ratingsAndReviews/ReviewsSummary.jsx
+++ product-detail-page/client/src/components/ratingsAndReviews/ReviewsSummary.jsx
@@ -6,7 +6,17 @@ import calculateStars from '../../lib/calculateStars.js';
 import calculatePercentage from '../../lib/calculatePercentage.js';
 import makeFilterMessage from '../../lib/makeFilterMessage.js';
 
+const possibleCharacteristics = [
+  {name: 'Size', lowLabel: 'too small', highLabel: 'too big'},
+  {name: 'Width', lowLabel: 'too narrow', highLabel: 'too wide'},
+  {name: 'Length', lowLabel: 'too short', highLabel: 'too long'},
+  {name: 'Comfort', lowLabel: 'poor', highLabel: 'great'},
+  {name: 'Quality', lowLabel: 'poor', highLabel: 'great'},
+  {name: 'Fit', lowLabel: 'poor', highLabel: 'great'}
+];
+
 const ReviewsSummary = ({ metaData, filters, filterClick, removeFilters }) => {
+  const characteristics = possibleCharacteristics.filter(c => metaData.characteristics[c.name]);
   return (
     <div className='rr-summary'>
       <div className='rr-summary-rollup'>
@@ -26,7 +36,9 @@ const ReviewsSummary = ({ metaData, filters, filterClick, removeFilters }) => {
         <BarGraph filterClick={ filterClick } star={ 1 } ratings={ metaData.ratings }/>
       </div>
       <div className='rr-percentage-recommended'>{calculatePercentage(metaData)}% of reviews recommend this product</div>
-      <Scale name={'test'} value={ 5 } lowLabel={'low'} highLabel={'high'} />
+      {characteristics.map(c => {
+        return <Scale key={metaData.characteristics[c.name].id} name={c.name} value={metaData.characteristics[c.name].value} lowLabel={c.lowLabel} highLabel={c.highLabel} />
+      })}
     </div>
   )
 };
