06b6118e825b60b3f962ac875d6058c1d1631856
Andre Bagur <andre.bagur@gmail.com>
2023-05-18 10:33:15 -0500

    minor changes to update questions and answers

--- product-detail-page/client/src/components/questionsAndAnswers/controllerQ&A.js
+++ product-detail-page/client/src/components/questionsAndAnswers/controllerQ&A.js
@@ -4,9 +4,9 @@ const axios = require('axios');
 module.exports = {
   getQuestions: (productID, page, count) => {
     // check inputs before making the request
-    //if (typeof productID !== 'number' || typeof parseInt(productID) !== 'number') productID = 37315;
-    //if (page && (typeof page !== 'number' || typeof parseInt(page) !== 'number')) page = 1;
-    //if (count && (typeof count !== 'number' || typeof parseInt(count) !== 'number')) count = 5;
+    if (typeof productID !== 'number' || typeof parseInt(productID) !== 'number') productID = 37315;
+    if (page && (typeof page !== 'number' || typeof parseInt(page) !== 'number')) page = 1;
+    if (count && (typeof count !== 'number' || typeof parseInt(count) !== 'number')) count = 5;
 
 
     return axios.get('http://localhost:3000/qa/questions', {
