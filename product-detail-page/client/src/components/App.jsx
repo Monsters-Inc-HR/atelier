2bc53d9338b85d41729abf3803c5b56355aaa32b
Alex Caron <alex.caron@gmail.com>
2023-05-18 14:29:36 -0500

    change logo

--- product-detail-page/client/src/components/App.jsx
+++ product-detail-page/client/src/components/App.jsx
@@ -8,7 +8,7 @@ import Controller from './relatedItems/controller.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
-const siteName = 'BUY BUY BUY';
+const siteName = 'Atelier';
 const siteWideAnnouncement = '20% off all winter styles  -  Check out new season styles  -  New fabrics are here!';
 
 
